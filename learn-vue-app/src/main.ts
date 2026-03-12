b56bf6513e2a68f25ec02b78869e8ccd0038f457
Success Daniel <sudaniel@2-D-6.42heilbronn.de>
2026-03-12 15:11:22 +0100

    Understand VDOM, components, directives

--- learn-vue-app/src/main.ts
+++ learn-vue-app/src/main.ts
@@ -1,24 +1,48 @@
-import { createApp, type ComponentOptions } from 'vue'
-
-type Data = {
-    counter: number;
-}
+import { createApp } from 'vue'
 
 const App = {
-    template: "<div> Counter: {{ counter }} </div>",
-    data(): Data {
-        return { counter: 0 }
-    },
-    created() {
-        const interval = setInterval(() => {
-            (this as ComponentOptions<Data>).counter++
-        }, 1000);
-        
-        setTimeout(() => {
-            clearInterval(interval)
-        }, 5000)
+    template: `
+        <ul>
+            <li
+            v-for="image in images"
+            :key="image.id"
+            :style="selected === image.id ? { border: '1px solid blue' } : {}"
+            @click="selected = image.id"
+            v-memo="[selected === image.id]"
+            >
+            <img :src="image.url">
+            <div>{{image.title}}</h2>
+            </li>
+            </ul>
+    `,
+    data() {
+        return {
+            selected: null,
+            images: [{
+            id: 1,
+            title: 'Cute cat',
+            url:
+            'https://res.cloudinary.com/mayashavin/image/upload/w_100,h_100,c_thumb/TheCute%20Cat',
+            }, {
+            id: 2,
+            title: 'Cute cat no 2',
+            url:
+            'https://res.cloudinary.com/mayashavin/image/upload/w_100,h_100,c_thumb/cute_cat',
+            }, {
+            id: 3,
+            title: 'Cute cat no 3',
+            url:
+            'https://res.cloudinary.com/mayashavin/image/upload/w_100,h_100,c_thumb/cat_me',
+            }, {
+            id: 4,
+            title: 'Just a cat',
+            url:
+            'https://res.cloudinary.com/mayashavin/image/upload/w_100,h_100,c_thumb/cat_1',
+            }]
+        }
     }
-};
+}
 
 const app = createApp(App)
-app.mount('#app')
+app.mount("#app");
+
